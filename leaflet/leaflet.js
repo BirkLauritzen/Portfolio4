b2754ec8e26ebb0707a5ac485210a98bf7f95fb6
abeldalum <abeldalum@hotmail.com>
2023-11-17 10:48:11 +0100

    ggg

--- leaflet/leaflet.js
+++ leaflet/leaflet.js
@@ -13,10 +13,10 @@ const tiles = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y
 }).addTo(map);
 
 const limeGreenIcon = L.icon({
-    iconUrl: '../pictures/alien.png', // Replace with your lime green marker icon path
-    iconSize: [25, 25], // Replace with your desired icon size
-    iconAnchor: [32, 32], // Replace with your desired icon anchor
-    popupAnchor: [-19, -30] // Replace with your desired popup anchor
+    iconUrl: '../pictures/alien.png',
+    iconSize: [25, 25],
+    iconAnchor: [32, 32],
+    popupAnchor: [-19, -30]
 });
 
 ufoSightingsArray.forEach(function (sighting) {
@@ -24,7 +24,6 @@ ufoSightingsArray.forEach(function (sighting) {
     const lat = parseFloat(sighting.latitude);
     const lng = parseFloat(sighting.longitude);
 
-    // Create a marker for each sighting and add it to the map
     const marker = L.marker([lat, lng], { icon: limeGreenIcon }).addTo(map);
     marker.bindPopup(`<br>Location: ${sighting.location}<br>datetime: ${sighting.datetime}`);
 });
